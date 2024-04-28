ef18480c329c9c6c13e983cf1ba7d0fc45f94cc9
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-04-28 15:13:52 +0430

    added style for about component

--- src/components/Fleet.js
+++ src/components/Fleet.js
@@ -8,15 +8,10 @@ const Fleet = () => {
       <div className="fleet-title">
         <h1>Our Fleet</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          At Ryan Transportation Services . we maintain a fleet Of luxurious and
+          well-maintained vehicles, including sedans. SUVs. limousines. and
+          more. Our vehicles are equipped with all the latest amenities to
+          ensure your comfort and safety.
         </p>
       </div>
       <div className="fleet-images">
